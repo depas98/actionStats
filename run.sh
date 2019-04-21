04561382f3c661900e68739cce8270b443b944e3
depas <depas98@hotmail.com>
2019-04-21 16:46:59 -0600

    Added scripts for running program

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-javac -d ./bin  -cp ./lib/jackson-core-2.9.7.jar;./lib/jackson-databind-2.9.7.jar;./lib/jackson-annotations-2.9.7.jar ./src/com/depas98/assignment/data/Action.java  ./src/com/depas98/assignment/data/ActionStat.java  ./src/com/depas98/assignment/data/ActionTime.java ./src/com/depas98/assignment/ActionStatsService.java
+javac -d ./bin  -cp ./lib/jackson-core-2.9.7.jar:./lib/jackson-databind-2.9.7.jar:./lib/jackson-annotations-2.9.7.jar ./src/com/depas98/assignment/data/Action.java ./src/com/depas98/assignment/data/ActionStat.java ./src/com/depas98/assignment/data/ActionTime.java ./src/com/depas98/assignment/ActionStatsService.java
 
-java  -cp ./bin;./lib/jackson-core-2.9.7.jar;./lib/jackson-databind-2.9.7.jar;./lib/jackson-annotations-2.9.7.jar com.depas98.assignment.ActionStatsService
+java -cp ./bin;./lib/jackson-core-2.9.7.jar:./lib/jackson-databind-2.9.7.jar:./lib/jackson-annotations-2.9.7.jar com.depas98.assignment.ActionStatsService
