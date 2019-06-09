f30f7d8280b75acaed0161c6d00e1af4f5e7a4df
depas <depas98@hotmail.com>
2019-06-08 18:54:33 -0600

    LAtest changes

--- runTests.sh
+++ runTests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-javac -d ./bin -cp ./lib/jackson-core-2.9.7.jar:./lib/jackson-databind-2.9.7.jar:./lib/jackson-annotations-2.9.7.jar:./bin:./lib/junit-4.12.jar:./lib/hamcrest-core-1.3.jar ./src/com/depas98/assignment/data/Action.java ./src/com/depas98/assignment/data/ActionStat.java ./src/com/depas98/assignment/data/ActionTime.java ./src/com/depas98/assignment/ActionStatsService.java ./test/com/depas98/assignment/ActionStatServiceTest.java 
+echo off
+javac -d ./bin -cp ./bin;./lib/junit-4.12.jar:./lib/hamcrest-core-1.3.jar ./src/com/depas98/security/SecurityMonitor.java ./src/com/depas98/security/SecurityMonitorService.java ./src/com/depas98/security/SecurityMonitorServiceImpl.java ./src/com/depas98/security/SecurityFileReaderTask.java  ./src/com/depas98/security/SecurityStatsWriterTask.java  ./src/com/depas98/security/SecurityType.java ./test/com/depas98/security/SecurityMonitorServiceTest.java 
 
-java -cp ./bin:./lib/junit-4.12.jar:./lib/jackson-core-2.9.7.jar:./lib/jackson-databind-2.9.7.jar:./lib/jackson-annotations-2.9.7.jar:./lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore com.depas98.assignment.ActionStatServiceTest
+java -cp ./bin;./lib/junit-4.12.jar:./lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore com.depas98.security.SecurityMonitorServiceTest
--- src/com/depas98/assignment/data/Action.java
+++ src/com/depas98/assignment/data/Action.java
@@ -1,6 +1,34 @@
 package com.depas98.assignment.data;
 
 public enum Action {
-    jump,
-    run
+    jump("jump"),
+    run("run"),
+    unknown("unknown");
+
+    private String name;
+
+    Action(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public static Action getActionByName(String name){
+        if (name==null){
+            throw new IllegalArgumentException("Name can't be null");
+        }
+
+        if (jump.getName().equalsIgnoreCase(name)){
+                return jump;
+        }
+        if (run.getName().equalsIgnoreCase(name)){
+            return run;
+        }
+        else{
+            return unknown;
+        }
+
+    }
 }
